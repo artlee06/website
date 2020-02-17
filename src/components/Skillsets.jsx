94303335432f4a281e92621d47c6f7f0b9430d4c
Arthur Lee <ykarthurlee@gmail.com>
2020-02-18 02:44:47 +0800

    first commit

---
+++ src/components/Skillsets.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "@emotion/styled";
+// import colors from "styles/colors";
+import Figma from '../svgs/figma.svg';
+import Illustrator from '../svgs/illustrator.svg';
+import Java from '../svgs/java.svg';
+import Photoshop from '../svgs/photoshop.svg';
+import Rails from '../svgs/rails.svg';
+import ReactNative from '../svgs/reactNative.svg';
+import XD from '../svgs/xd.svg';
+
+//can consider having an expand to see more such that it reveals the proficiency level and stuff
+const IconInfo = (props) => {
+    const { svg, text } = props;
+    const IconContainer = styled("div")`
+        margin-top: 1.5em;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    `;
+    const IconText = styled("a")`
+        margin-top: 0.25em;
+        margin-bottom: 1em;
+        font-weight: 450;
+        font-size: small;
+        line-height: 1.9;
+        text-decoration: none;
+        color: black;
+        align-self: center;
+    `;
+    const Icon = styled("svg")`
+        align-self: center;
+    `;
+    return (
+      <IconContainer>
+        {/* <Icon>  */}
+            {svg}
+        {/* </Icon> */}
+        <IconText> 
+            {text}
+        </IconText>    
+      </IconContainer>
+    );
+  };
+
+const SkillsContainer = styled.div`
+    width: auto;
+    height: auto;
+    background-color: whitesmoke;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+`;
+
+const Container = styled.div`
+    background-color: white;
+    width: auto;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.06);
+`;
+
+const Title = styled("h3")`
+    font-weight: bold;
+    text-decoration: none;
+    color: black;
+    align-self: center;
+    margin-bottom: 1em;
+`;
+
+export default function Skillsets() {
+  return (
+    <React.Fragment>
+        <Container> 
+            <Title> 
+                My Skillsets
+            </Title>
+            <SkillsContainer>
+                {/* <ReactIcon /> */}
+                {/* <Rails /> */}
+                {/* <Photoshop className="item" /> */}
+                {/* <Illustrator className="item"/> */}
+                {/* <XD /> */}
+                <IconInfo svg={<ReactNative />} text="React / React Native" />
+                <IconInfo svg={<Rails />} text="Ruby On Rails" />
+                <IconInfo svg={<XD />} text="Adobe XD" />
+                <IconInfo svg={<Figma />} text="Figma" />
+                <IconInfo svg={<Illustrator />} text="Adobe Illustrator" />
+                <IconInfo svg={<Photoshop />} text="Adobe Photoshop" />
+            </SkillsContainer>
+        </Container>
+    </React.Fragment>
+  );
+}
+
---
+++ src/components/_ui/IconsObject.jsx
@@ -0,0 +1,7 @@
+import figma from "src/images/figma-1.svg";
+
+const IconsObject = {
+    figma: figma
+};
+
+export default IconsObject;
