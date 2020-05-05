9d84aee0bbb96b0f1c569333e1bd5964e4f67649
Arthur Lee <ykarthurlee@gmail.com>
2020-05-05 23:58:52 +0800

    Added empty components for building

---
+++ src/components/_ui/RoundButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function RoundButton(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+RoundButton.propTypes = {
+
+}
+
+export default RoundButton
+
---
+++ src/components/sections/ContactSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ContactSection(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+ContactSection.propTypes = {
+
+}
+
+export default ContactSection
+
